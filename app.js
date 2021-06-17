580e586ec7a9eda97b3173e0276722be77ccf887
yenhoang <japocoin@gmail.com>
2021-06-17 16:29:29 +0200

    allDone and allUnDone action added

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-const app = new Vue ({
-  el: '#app', 
+const app = new Vue({
+  el: "#app",
   data: {
-    title: 'To Do App', 
-    newTodo: '', 
-    todos: []
-
-  }, 
+    title: "To Do App",
+    newTodo: "",
+    todos: [],
+  },
   methods: {
     addTodo() {
-      console.log('form submitted');
+      console.log("form submitted");
       this.todos.push({
-        title: this.newTodo, 
-        done: false
+        title: this.newTodo,
+        done: false,
       });
-      this.newTodo=''
-    }, 
+      this.newTodo = "";
+    },
     removeTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos.splice(todoIndex, 1);
-    }
-  }
-})
+    },
+    allDone() {
+      this.todos.forEach((todo) => {
+        todo.done = true;
+      });
+    },
+    allUnDone() {
+      this.todos.forEach((todo) => {
+        todo.done = false;
+      });
+    },
+  },
+});
