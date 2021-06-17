9e142d6b33d496c6ee1e769bad98518b7074c7a0
yenhoang <japocoin@gmail.com>
2021-06-17 19:01:07 +0200

    edit action added

--- app.js
+++ app.js
@@ -4,29 +4,49 @@ const app = new Vue({
     title: "To Do App",
     task: "",
     tasks: [],
+    editedTask: null,
   },
   methods: {
+
+    //add / update task 
     addTask() {
-      console.log("form submitted");
-      this.tasks.push({
-        title: this.task,
-        done: false,
+      if (this.task.length === 0) return;
+
+      // for updating a task
+      if(this.editedTask != null) {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      }else{
+        //to only add a task
+        this.tasks.push({
+          name: this.task,
+          done: false,
       });
+      }
+      
       this.task = "";
     },
-    removeTodo(task) {
-      const taskIndex = this.tasks.indexOf(task);
-      this.tasks.splice(taskIndex, 1);
+
+    removeTask(index) {
+      //const taskIndex = this.tasks.indexOf(task);
+      this.tasks.splice(index, 1);
     },
+
     allDone() {
       this.tasks.forEach((task) => {
         task.done = true;
       });
     },
+
     allUnDone() {
       this.tasks.forEach((task) => {
         task.done = false;
       });
+    },
+
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
     },
   },
 });
