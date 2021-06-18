fd92ae91287b7755e6999db741452099ffa5c2ac
yenhoang <japocoin@gmail.com>
2021-06-18 13:43:52 +0200

    indentation fixed

--- app.js
+++ app.js
@@ -7,23 +7,22 @@ const app = new Vue({
     editedTask: null,
   },
   methods: {
-
-    //add / update task 
+    //add / update task
     addTask() {
       if (this.task.length === 0) return;
 
       // for updating a task
-      if(this.editedTask != null) {
+      if (this.editedTask != null) {
         this.tasks[this.editedTask].name = this.task;
         this.editedTask = null;
-      }else{
+      } else {
         //to only add a task
         this.tasks.push({
           name: this.task,
           done: false,
-      });
+        });
       }
-      
+
       this.task = "";
     },
 
@@ -32,12 +31,12 @@ const app = new Vue({
       this.tasks.splice(index, 1);
     },
 
-    sortUndone () {
-      this.tasks.sort((a,b) => a.done > b.done ? 1 : -1);
+    sortUndone() {
+      this.tasks.sort((a, b) => (a.done > b.done ? 1 : -1));
     },
 
-    sortDone () {
-      this.tasks.sort((a,b) => a.done < b.done ? 1 : -1);
+    sortDone() {
+      this.tasks.sort((a, b) => (a.done < b.done ? 1 : -1));
     },
 
     allDone() {
@@ -56,5 +55,5 @@ const app = new Vue({
       this.task = this.tasks[index].name;
       this.editedTask = index;
     },
-  }
+  },
 });
